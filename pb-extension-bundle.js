e02e7fdd3a1d265a682e29e2fd64216eea4b666d
Kalo <calogero.g.1996@gmail.com>
2025-08-05 18:04:08 +0200

    Added dependencies

--- pb-extension-bundle.js
+++ pb-extension-bundle.js
@@ -3,12 +3,10 @@ import './src/pb-tify-v2.js';
 
 import "./src/components/load/inputBar.js";
 import "./src/components/load/manifestImport.js";
-
 import "./src/components/buttons/manifestExport.js";
 import "./src/components/buttons/manifestURLCopy.js";
 import "./src/components/buttons/close.js";
 import "./src/components/buttons/languageSelector.js";
-
 import "./src/components/buttons/snap/snapshot.js";
 import "./src/components/buttons/snap/snapForm.js";
 import "./src/components/buttons/snap/form/region.js";
@@ -16,12 +14,10 @@ import "./src/components/buttons/snap/form/rotation.js";
 import "./src/components/buttons/snap/form/size.js";
 import "./src/components/buttons/snap/form/qualityFormat.js";
 import "./src/components/buttons/snap/form/preview.js";
-
 import "./src/components/viewer/tifyWrapper.js";
 import "./src/components/viewer/metadata.js";
 import "./src/components/viewer/pageMetadata.js";
 import "./src/components/viewer/title.js";
-
 import "./src/components/annotations/annotations.js";
 import "./src/components/annotations/form.js";
 import "./src/components/annotations/frame.js";
@@ -36,3 +32,16 @@ import "./src/components/annotations/buttons/export.js";
 import "./src/components/annotations/buttons/filter.js";
 import "./src/components/annotations/buttons/hideall.js";
 import "./src/components/annotations/buttons/import.js";
+
+import "./src/utilities/base.js";
+import "./src/utilities/config.js";
+import "./src/utilities/lib/API.js";
+import "./src/utilities/lib/db.js";
+import "./src/utilities/lib/languages.js";
+import "./src/utilities/lib/manifest.js";
+import "./src/utilities/lib/parse.js";
+import "./src/utilities/lib/utils.js";
+
+import "./src/style/main.css";
+import "./src/public/img/logo.png";
+import "./src/public/img/logo_text.png";
