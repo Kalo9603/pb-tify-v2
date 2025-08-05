fd528973a1ead956c510dc1148f07d353c9525b2
Kalo <calogero.g.1996@gmail.com>
2025-08-05 17:37:28 +0200

    Renames and added imports

--- pb-extension-bundle.js
+++ pb-extension-bundle.js
@@ -1,21 +1,38 @@
-/*
- * Add your components here to include them in the generated bundles
- */
 
 import './src/pb-tify-v2.js';
 
 import "./src/components/load/inputBar.js";
 import "./src/components/load/manifestImport.js";
 
-import "./src/components/annotations/annotations.js";
-
 import "./src/components/buttons/manifestExport.js";
 import "./src/components/buttons/manifestURLCopy.js";
 import "./src/components/buttons/close.js";
 import "./src/components/buttons/languageSelector.js";
+
 import "./src/components/buttons/snap/snapshot.js";
+import "./src/components/buttons/snap/snapForm.js";
+import "./src/components/buttons/snap/form/region.js";
+import "./src/components/buttons/snap/form/rotation.js";
+import "./src/components/buttons/snap/form/size.js";
+import "./src/components/buttons/snap/form/qualityFormat.js";
+import "./src/components/buttons/snap/form/preview.js";
 
 import "./src/components/viewer/tifyWrapper.js";
 import "./src/components/viewer/metadata.js";
 import "./src/components/viewer/pageMetadata.js";
 import "./src/components/viewer/title.js";
+
+import "./src/components/annotations/annotations.js";
+import "./src/components/annotations/form.js";
+import "./src/components/annotations/frame.js";
+import "./src/components/annotations/view.js";
+import "./src/components/annotations/wrapper.js";
+import "./src/components/annotations/buttons/add.js";
+import "./src/components/annotations/buttons/coordinates.js";
+import "./src/components/annotations/buttons/delete.js";
+import "./src/components/annotations/buttons/duplicate.js";
+import "./src/components/annotations/buttons/edit.js";
+import "./src/components/annotations/buttons/export.js";
+import "./src/components/annotations/buttons/filter.js";
+import "./src/components/annotations/buttons/hideall.js";
+import "./src/components/annotations/buttons/import.js";
